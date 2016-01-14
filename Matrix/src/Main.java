5768547583b47b17ff70405738fdb81f48acef9e
AdiAdelina <iorgaadelina@yahoo.com>
2016-01-14 08:46:57 +0200

    added min,elemente pare

--- Matrix/src/Main.java
+++ Matrix/src/Main.java
@@ -14,9 +14,26 @@ public class Main {
 
 		PrintWriter writer = new PrintWriter("Matrix/src/out.txt", "UTF-8");
 		
+		int[][] matrix = readMatrix.getMatrix();
+		
 		writer.print(readMatrix.toString());
 		writer.println("Suma matrice= "+workSquare.SumaMatrix());
+		writer.println( "Elementul maxim este "+ workSquare.Maxim());
+		writer.println("Elementul minim este "+workSquare.Minim());
+		writer.print("Numerele impare : ");
+		for (int i = 0; i < workSquare.AfisImpare(matrix).length; i++) 
+		{
+			writer.print(workSquare.AfisImpare(matrix)[i] + " ");
+		}
+		
+		for (int i = 0; i < workSquare.AfisPare(matrix).length; i++) 
+		{
+			writer.print(workSquare.AfisPare(matrix)[i] + " ");
+		}
+		
+		writer.println();
 		writer.println("ok");
 		writer.close();
 	}
+	
 }
--- Matrix/src/WorkMatrix.java
+++ Matrix/src/WorkMatrix.java
@@ -1,5 +1,4 @@
-
-
+import java.util.Arrays;
 
 public class WorkMatrix {
 	private int[][] matrix;
@@ -14,8 +13,16 @@ public class WorkMatrix {
 
 	public void setMatrix(int[][] matrix) {
 		this.matrix = matrix;
-		int max;
+		
 	}
+	int[] addElement(int[] org, int added) {
+		int[] result = Arrays.copyOf(org, org.length + 1);
+		result[org.length] = added;
+		return result;
+	}
+	
+	
+	
 	public int SumaMatrix(){
 		int s = 0;
 		for (int i = 0; i < matrix.length; i++) {
@@ -25,7 +32,64 @@ public class WorkMatrix {
 		}
 		return s;
 	
-	
 	}
 	
+	public int Maxim() 
+	{
+			int max= matrix[0][0];
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix.length; j++){
+					if (matrix[i][j]>max)
+						max=matrix[i][j];}
+				}
+			return max;
+	}
+	
+	
+	public int Minim()
+	{ int min=matrix[0][0];
+	for (int i=0; i<matrix.length;i++)
+	{
+		for (int j=0;j< matrix.length;j++)
+	{
+			if (matrix [i][j]<min)
+		min=matrix[i][j];}
+	}
+	return min;
+	} 
+	
+	
+	
+	
+	public int[] AfisImpare(int[][] matrix) // afiseaza elementele impare
+	{
+		int[] temp = new int[0];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				if (matrix[i][j] % 2 != 0) {
+					temp = addElement(temp, matrix[i][j]);
+				}
+			}
+		}
+		return temp;
+	}
+	
+	public int [] AfisPare (int[][]matrix) // afis elem pare
+	{ int [] par= new int [0];
+	for (int i=0;i<matrix.length;i++) {
+		for (int j=0; j<matrix.length; j++) {
+			if (matrix[i][j]%2==0)
+			{ par=addElement (par, matrix [i][j]);
+			}
+		}
+	}
+	return par;
+	}
+	
+	
+	
+	
 }
+	
+	
+	
